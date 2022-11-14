e56d29173fd7e24da799c263eb1b396ef3a87fa5
Bassam-Ullah <bassam.ullah@sourcefuse.com>
2022-11-14 19:07:40 +0530

    env file added

--- server/src/datasources/pg.datasource.ts
+++ server/src/datasources/pg.datasource.ts
@@ -1,15 +1,17 @@
 import {inject, lifeCycleObserver, LifeCycleObserver} from '@loopback/core';
 import {juggler} from '@loopback/repository';
+import * as dotenv from 'dotenv';
+dotenv.config();
 
 const config = {
   name: 'PG',
   connector: 'postgresql',
   // url: 'postgres://postgres:root@localhost/mydatabase',
-  host: 'localhost',
-  port: 5432,
-  user: 'postgres',
-  password: 'root',
-  database: 'mydatabase',
+  host: process.env.HOST,
+  port: +(process.env.PORT ?? 3000),
+  user: process.env.DBUSERNAME,
+  password: process.env.PASSWORD,
+  database: process.env.DATABASE,
 };
 
 // Observe application's life cycle to disconnect the datasource when
